f7398707a1285764e22bd52557b2f39d3241a5c5
DanielBietola <dbiet056@uottawa.ca>
2020-11-14 16:03:33 -0500

    Initial commit

---
+++ Assets/Help.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Help : MonoBehaviour
+{
+    public string url;
+    public void Open()
+    {
+        Application.OpenURL(url);
+    }
+}
